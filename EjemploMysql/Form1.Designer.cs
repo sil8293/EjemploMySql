38138e0c3e59c743dcc72cb298c4cc5794ab5239
sil8293 <sil_8293@hotmail.com>
2016-04-08 15:38:50 +0200

    pokedex beta

---
+++ EjemploMysql/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace EjemploMysql
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(51, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 64);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(128, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 160);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EjemploMysql/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//añadimos los import de mysql
+using System.Data.Odbc;
+using MySql.Data.Types;
+using MySql.Data.MySqlClient;
+
+
+namespace EjemploMysql
+{
+    public partial class Form1 : Form
+    {
+        //este ejemplo conectará con una base de datos 
+        //MYSQL
+
+        //necesito 5 parámetros:
+            //Server: la ip o npmbre dns del servidor
+            //Database: nonmbre de la base de datos
+            //Uid: usuario(OJO!!! no se puede dejar en blanco)
+            //Pwd: clave de acceso si la tuviera
+            //Port: default=3306
+
+        //paramatetro de la conexion
+        private string connStr;
+
+        //variable que maneja la conexion
+        private MySqlConnection conn;
+
+        //cosulta que quiero hacer a la base de datos
+        private String Sentencia_SQL;
+
+        //variable que sirve para crear la conexion
+        private static MySqlCommand comando;
+
+        //guarda el resultado de la consulta
+        private MySqlDataReader resultado;
+
+        //abre la base de datos y la guarda en DataTable() 
+        private DataTable datos = new DataTable();
+
+        private int contadorFila=0;
+        private int numeroTotalFilas = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+
+            connStr = "Server=localhost; Database=test; Uid=root; Pwd=root; Port=3306";
+            conn = new MySqlConnection(connStr);
+            //abrir la conexion
+            conn.Open();
+            Sentencia_SQL = "Select * from pokemon";
+            comando = new MySqlCommand(Sentencia_SQL, conn);
+            resultado = comando.ExecuteReader();
+            datos.Load(resultado);
+            conn.Close();
+            numeroTotalFilas = datos.Rows.Count;
+           
+        }
+        private String consulta(String columna) { 
+
+            
+            DataRow fila =datos.Rows[contadorFila];
+             
+
+             if (fila != null)
+             {
+                 return fila[columna].ToString();
+             }
+               
+             else return "no existe";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //si consigue leerun dato de la base de datos es que todo 
+            //esta OK
+
+
+            label1.Text = consulta("name");
+            label2.Text = consulta("habitat");
+            label3.Text = consulta("id");
+            contadorFila++;
+            if (numeroTotalFilas == contadorFila)
+            {
+                contadorFila = 0;
+
+            }
+          
+        }
+        
+    }
+}
